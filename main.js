a3b167de76c06cd064bb8039ece9f6ac2a6901c8
Ronald Hunter <ronald@ronaldhunter.co>
2016-04-20 15:19:46 -0400

    added overlay class rules to main.css. Appened overlay to body via jquery and made. When image clicked shows the overlay.

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 jQuery(document).ready(function($) {
 
+	var $overlay = $('<div class="overlay"></div>');
+	//Add overlay
+		$("body").append($overlay);
+		//Add Image.
+		//A Caption	
 
 
-	//1. Capture click event on a link to an Image.
+
+
+
+	//Capture click event on a link to an Image.
 	$('#imageGallery a').click(function(e) {
 		e.preventDefault();
 		var href = $(this).attr('href');
 
 		console.log(href);
-		//1.1 Show overlay.
-		//1.2 Update the overlay with the image linked in the link.
-		//1.3 Get childs alt attribute and add it to the caption.
+		//Show overlay.
+		$overlay.show();
+		//Update the overlay with the image linked in the link.
+		//Get childs alt attribute and add it to the caption.
 	});
 
 
-	//2. Add overlay
-		//2.1 Add Image.
-		//2.2 A Caption
-	//3. When overlay is clicked
-		//3.1 Hide the overlay
+
+	//When overlay is clicked
+		//Hide the overlay
 
 
 
